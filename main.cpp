d7406dd5910a25587f89e1b8048a2944cd24a190
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-12-08 06:02:29 -0800

    fix file selection bug

--- main.cpp
+++ main.cpp
@@ -10,40 +10,30 @@ const string directory = "C:\\Users\\Jereniah\\3D Objects\\Projects";
 int changeDirectory(string directory)
 {
     int changeResult = _chdir(directory.c_str());
-    if (changeResult != 0)
-    {
-        cout << "Failed to change directory" << endl;
-        return 1;
-    }
-    return 0;
+    return changeResult;
 }
-void pipeRead(FILE* listFilePipe, string& selectedPath)
+void pipeRead(FILE *listFilePipe, string &selectedPath)
 {
     char buffer[128];
     while (fgets(buffer, 128, listFilePipe) != NULL)
     {
+        buffer[strcspn(buffer,"\n")] = '\0';
         selectedPath += buffer;
     }
 }
 int openFile(string selectedPath)
 {
-    if (!selectedPath.empty()) {
-
-    string command = vscodeCommand + " " + selectedPath;
-    int result = system(command.c_str());
-
-    if (result != 0)
+    if (!selectedPath.empty())
     {
-        cout << "Failed to open vscode" << endl;
-        return 1;
-    }
-    return 0;
-    } else {
-        cerr << "No file selected" << endl;
-        return 1;
-    }
-}
+        string fullPath  = directory + "\\" + selectedPath;
+        cout<<fullPath<<endl;
 
+        string command = vscodeCommand + " \"" + fullPath + "\"";
+        int result = system(command.c_str());
+        return result;
+    }
+    return 1;
+}
 
 int main(int argc, char **argv)
 {
@@ -52,7 +42,7 @@ int main(int argc, char **argv)
         int changeResult = changeDirectory(directory);
         if (changeResult == 0)
         {
-            const string listFilesCommand = "fd --type d --color=never . | fzf --print-query";
+            const string listFilesCommand = "fd --type d --color=never . | fzf ";
             FILE *listFilePipe = _popen(listFilesCommand.c_str(), "r");
             // const string listFilesCommand = ("fd  --type f --type d --color=never . " + directory + "| fzf --print-query");
             if (!listFilePipe)
@@ -62,14 +52,18 @@ int main(int argc, char **argv)
             }
             string selectedPath;
             pipeRead(listFilePipe, selectedPath);
+            // selectedPath.erase(remove(selectedPath.begin(), selectedPath.end(), '\n'), selectedPath.end());
             if (_pclose(listFilePipe) != 0)
             {
                 cout << "Failed to close pipe" << endl;
                 return 1;
             }
-            // selectedPath.erase(remove(selectedPath.c_str().begin(), selectedPath.c_str().end(), '\n'), selectedPath.end());
-               }
+            int result = openFile(selectedPath);
+            if (result != 0)  {
+                return 1;
+            }
+            
+        }
     }
     return 0;
 }
-
